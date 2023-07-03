d95f6cfc18137c43ae5d74a8fe64b8ea92630988
Jarred Mac Intosh <jarredmacintosh9@gmail.com>
2023-07-03 14:52:22 +0200

    When the user selects a new status, update the pet list with the new data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
+// import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,9 @@ import { MatListModule } from '@angular/material/list';
     MatCardModule,
     MatSelectModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    FormsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pet-list/pet-list.component.ts
+++ src/app/components/pet-list/pet-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
+import { Observable } from 'rxjs';
 import { Pet } from 'src/app/interfaces/pets.interface';
 import { PetsService } from 'src/app/services/pets.service';
 import { Status } from 'src/app/stats.enum';
@@ -12,15 +14,32 @@ import { Status } from 'src/app/stats.enum';
   styleUrls: ['./pet-list.component.scss']
 })
 export class PetListComponent implements OnInit {
-
+  /** Data for pets displayed in the list. */
   public pets: Pet[] = [];
+  /** Fill the select box with the enum values. */
+  public statusCodes = Status;
+  /** Sets the initial selected value in the select box to available. */
+  public initialValue = Status.AVAILABLE;
 
   constructor(private petsService: PetsService) {}
 
   ngOnInit(): void {
-    this.petsService.getPetsByStatus(Status.AVAILABLE).subscribe(data => {
-      console.log('data', data);
+    this.getPetData(this.initialValue).subscribe(data => {
       this.pets = data;
     });
+  }
+
+  /**
+   * When the user selects a new 
+   * @param event MatSelectChange event.
+   */
+  public onStatusChange(event: MatSelectChange): void {
+    this.getPetData(event.value).subscribe(data => {
+      this.pets = data;
+    });
+  }
+
+  private getPetData(status: Status): Observable<Pet[]> {
+    return this.petsService.getPetsByStatus(status);
   }
 }
